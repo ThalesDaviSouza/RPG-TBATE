0976ca3fd59d54ff24545d4b403a72166084a361
Thales Davi de Souza <thalesdavisoul@gmail.com>
2025-03-02 12:40:44 -0300

    Trocando entre tabs

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const { createApp, ref } = Vue
 
+
 createApp({
   setup() {
+    const pages = {
+      homePage: 0,
+      perfilPage: 1,
+    }
+
     const message = ref('Hello vue!')
+    const actualPage = ref(pages.homePage)
     
+    function setPageTab(page){
+      actualPage.value = page;
+    }
+
     return {
-      message
+      // Refs
+      message,
+      actualPage,
+      pages,
+
+      // Functions
+      setPageTab,
     }
   }
 }).mount('#app')
