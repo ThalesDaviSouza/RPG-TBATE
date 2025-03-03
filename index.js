66bfbdd02c488e9902a8fab3874d147475c1dc55
Thales Davi de Souza <thalesdavisoul@gmail.com>
2025-03-03 14:19:16 -0300

    Permitindo adicionar e editar modificadores

--- index.js
+++ index.js
@@ -50,6 +50,8 @@ createApp({
     function modifierFactory(){
       return {
         element: null,
+        elementId: null,
+        effectType: null,
         nivel: null,
       }
     }
@@ -72,7 +74,123 @@ createApp({
     }
 
     function hasElement(elementId){
-      return character.elements.find(elem => elem.id == elementId) != undefined;
+      return character.elements.find(elem => elem.id == elementId) != undefined && character.elements.find(elem => elem.id == elementId).nivel > 0;
+    }
+    function getElementNivel(elementId){
+      return character.elements.find(elem => elem.id == elementId).nivel;
+    }
+    
+    function getElement(elementId){
+      return elements.find((elem, id) => id == elementId);
+    }
+
+    function updateModifiers(modifierId, effectType, elementId, nivel){
+      modifiers.value[modifierId].element = getElement(elementId);
+      modifiers.value[modifierId].elementId = elementId;
+      modifiers.value[modifierId].effectType = effectType;
+      modifiers.value[modifierId].nivel = nivel;
+    }
+
+    function modificadorIsDefined(modifier){
+      return modifier.element != null && modifier.effectType != null && modifier.nivel != null;
+    }
+
+    function getElementEffect(elementId, nivel){
+      let elem = getElement(elementId);
+      if(elem != undefined){
+        let effectData = undefined;
+
+        switch (elem.type) {
+          case elementTypes.basic:
+            effectData = basicEffects.find(effect => effect.tier == nivel);
+            break;
+        
+          case elementTypes.desviantSimple:
+            effectData = desviantSimpleEffects.find(effect => effect.tier == nivel);
+            break;
+
+          case elementTypes.desviantComplex:
+            effectData = desviantComplexEffects.find(effect => effect.tier == nivel);
+            break;
+        
+          default:
+            break;
+        }
+
+        return effectData;
+      }
+    }
+
+    function clearModifiers(){
+      modifiers.value = [];
+    }
+
+    function getManaTotalCost(){
+      let totalCost = 0;
+      for(let i = 0; i < modifiers.value.length; i++){
+        let mod = modifiers.value[i]
+
+        if(!modificadorIsDefined(mod)){
+          continue;
+        }
+        
+        let effect = getElementEffect(mod.elementId, mod.nivel)
+        if(mod.effectType == effectTypes.dano){
+          totalCost += effect.cost;
+        }
+        else{
+          totalCost += effect.costSecondary;
+        }
+      }
+
+      return totalCost;
+    }
+
+    function getModifiersResume(){
+      let resume = '';
+      let totalBuff = 0;
+      let totalDices = '';
+      let area = '';
+
+      for(let i = 0; i < modifiers.value.length; i++){
+        let mod = modifiers.value[i]
+
+        if(!modificadorIsDefined(mod)){
+          continue;
+        }
+        
+        let effect = getElementEffect(mod.elementId, mod.nivel)
+        if(mod.effectType == effectTypes.dano){
+          if(totalDices.length != 0){
+            totalDices += ' + '
+          }
+          totalDices += effect.dano;
+        }
+        else if(mod.effectType == effectTypes.buff){
+          totalBuff += effect.buff;
+        }
+        else if(mod.effectType == effectTypes.danoSecondary){
+          if(totalDices.length != 0){
+            totalDices += ' + '
+          }
+          totalDices += effect.danoSecondary;
+        }
+        else if(mod.effectType == effectTypes.area){
+          area += effect.area;
+        }
+      }
+      
+      resume += totalDices;
+
+      if(totalBuff > 0){
+        resume += ` + ${totalBuff}`;
+      }
+
+      if(area != ''){
+        resume += ` | ${area}`;
+      }
+
+      return resume;
     }
 
     watch(character, async (newValue) => {
@@ -105,9 +223,17 @@ createApp({
       addModifier,
       updateElements,
       hasElement,
+      updateModifiers,
+      getElementNivel,
+      modificadorIsDefined,
+      getElementEffect,
+      clearModifiers,
+      getManaTotalCost,
+      getModifiersResume,
     }
   },
   mounted(){
     loadCharacterData(this);
+    // preparerMultilevelDropdown();
   }
 }).mount('#app')
